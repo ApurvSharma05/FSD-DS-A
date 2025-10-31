84b38ee45d0f35a4b585ba942c2d5fa24caa750a
ApurvSharma05 <apurvsharma0514@gmail.com>
2025-10-31 11:50:26 +0530

    react

---
+++ ClassWork/src/App.jsx
@@ -0,0 +1,15 @@
+import Car from './vehicle'
+import './App.css'
+function App() {
+
+  return (
+    <>
+     <Car color="Red" brand="jeep"/>
+     <h1>testing component behavior</h1>
+     <h2>Testing</h2>
+     <Car color="Black" brand="Ford"/>
+    </>
+  )
+}
+
+export default App
